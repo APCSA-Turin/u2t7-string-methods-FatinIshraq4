a3afff58bd1b6016cd7521058b1ce7370f29c720
FatinIshraq4 <fatini5@nycstudents.net>
2024-10-09 15:44:36 +0000

    hi

---
+++ Lab3.java/CustomStringMethods.java
@@ -0,0 +1,31 @@
+public class CustomStringMethods {
+    
+    public CustomStringMethods() { }
+
+    public boolean longerThan(String myString, int maxLength) {
+        if (myString.length()<=maxLength) {
+            return false;
+        } else {
+            return true;
+        }
+    
+    }
+
+    public String funnyString(String str, int idx) {
+        String x = str.substring(idx, idx + 1);
+        str = x + str + x;
+        return str;
+    }
+
+    public String halvesReversed(String myString) {
+        String firstHalf = myString.substring(0, myString.length()/2);
+        String secondHalf = myString.substring(myString.length()/2);
+        String reversedHalves = secondHalf + firstHalf;
+        return reversedHalves;
+    }
+  
+  
+
+
+    
+}
