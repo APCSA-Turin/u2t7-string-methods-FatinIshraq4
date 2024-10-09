907033fa26482d667125ae4704b8ea4ff5588735
FatinIshraq4 <fatini5@nycstudents.net>
2024-10-09 16:35:07 +0000

    hi

--- Lab3.java/CustomStringMethods.java
+++ Lab3.java/CustomStringMethods.java
@@ -23,6 +23,68 @@ public class CustomStringMethods {
         String reversedHalves = secondHalf + firstHalf;
         return reversedHalves;
     }
+
+    public String pigLatin(String orig) {
+        String x = orig.substring(0,1);
+        orig = orig.substring(1) + x + "ay";
+        return orig;
+    }
+
+    public String removeCharacter(String myString, int removeIdx) {
+        if ((removeIdx + 1)>myString.length()) {
+            return myString;
+        } else {
+            String x = myString.substring(removeIdx + 1);
+            String y = myString.substring(0,removeIdx);
+            myString = y + x;
+            return myString;
+        }
+        
+    }
+
+    public String insertAt(String orig, String insertText, String searchStr) {
+        if (orig.contains(searchStr)) {
+            int x = orig.indexOf(searchStr);
+            String y = orig.substring(0, x);
+            String z = orig.substring(x);
+            orig = y + insertText + z;
+            return orig;
+        } else {
+            orig = orig + insertText;
+            return orig;
+        }
+    }
+
+    public String endUp(String myString, int numToCap){
+        if (numToCap<=myString.length()) {
+            int z = myString.length() - numToCap;
+            String x = myString.substring(z);
+            String y = myString.substring(0, z);
+            x = x.toUpperCase();
+            myString = y + x;
+            return myString;
+        } else {
+            myString = myString.toUpperCase();
+            return myString;
+        }    
+    }
+  
+    public String yellOrWhisper(String myString) {
+        int index = 0;
+        char x = myString.charAt(index);
+        boolean y = Character.isUpperCase(x);
+        if (y = true) {
+            myString = myString.toUpperCase();
+            return myString;
+        } else {
+            myString = myString.toLowerCase();
+            return myString;
+        }
+    }
+  
+  
+  
+  
   
   
 
--- Lab3.java/CustomStringMethodsRunner.java
+++ Lab3.java/CustomStringMethodsRunner.java
@@ -1,28 +1,22 @@
 public class CustomStringMethodsRunner {
     public static void main(String[] args) {
         CustomStringMethods methods = new CustomStringMethods();
+        String str = methods.yellOrWhisper("Hello James!");
+        System.out.println(str);
+        str = methods.yellOrWhisper("hello James!");
+        System.out.println(str);
+        str = methods.yellOrWhisper("aBCDEFGHIJK");
+        System.out.println(str);
+        str = methods.yellOrWhisper("Abcdefghijk");
+        System.out.println(str);
+        str = methods.yellOrWhisper("B");
+        System.out.println(str);
+        str = methods.yellOrWhisper("b");
+        System.out.println(str);
+        str = methods.yellOrWhisper("IT'S SUNNY!");
+        System.out.println(str);
+        str = methods.yellOrWhisper("it's rainy");
+        System.out.println(str);
         
-
-        
-        
-        String str = methods.halvesReversed("computer");  //even # of letters
-        System.out.println(str);
-        str = methods.halvesReversed("computers");        //odd # of letters
-        System.out.println(str);
-        str = methods.halvesReversed("sky");
-        System.out.println(str);
-        str = methods.halvesReversed("weekend");
-        System.out.println(str);
-        str = methods.halvesReversed("an");
-        System.out.println(str);
-        str = methods.halvesReversed("I");
-        System.out.println(str);
-        str = methods.halvesReversed("see you soon");
-        System.out.println(str);
-        str = methods.halvesReversed("see you later!");
-        System.out.println(str);
-
-
-
     }
 }
